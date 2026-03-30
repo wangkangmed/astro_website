5931b6ef007ac3ec519be18a4f237981fcbf8697
wangkangmed <ai.wangkang.med@gmail.com>
2026-03-30 23:29:50 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-// astro.config.mjs
+
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://wangkangmed.github.io', 
-  base: '/ai-tech-blog', 
-
+  site: 'https://wangkangmed.github.io',
+  base: '/astro_website', // 必须和你的仓库名完全一致
   integrations: [mdx(), sitemap()],
 });
